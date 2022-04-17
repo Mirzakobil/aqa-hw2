8694f49b28a4f803499989c5983c319b08c2be65
Mirzakobil <mirzakobilanvarov@gmail.com>
2022-04-17 11:46:57 +0500

    first part done

--- specs/login-layout.e2e.js
+++ specs/login-layout.e2e.js
@@ -1,16 +1,32 @@
 // REMOVE THE BELOW CODE BEFORE START THE EXERCISE
 describe('Check app', function () {
-    it('should login', async function () {
-        await browser.url('https://viktor-silakov.github.io/course-sut');
-        await $('#login').setValue('walker@jw.com');
-        await $('#password').setValue('password');
-        await $('button').click();
-        await browser.pause(15000);
-        const title = await browser.getTitle();
-        if (title !== 'Report portal') {
-            throw new Error('You don`t login into system!!!')
-        }
+  it('should login', async function () {
+    await browser.url('https://viktor-silakov.github.io/course-sut');
+    await $('#login').setValue('walker@jw.com');
+    await $('#password').setValue('password');
+    await $('button').click();
+  });
+
+  it('should check spinner', async function () {
+    const spinnerHidden = await $('#spinner').waitForDisplayed({
+      reverse: true,
     });
+  });
+
+  it('should verify user', async function () {
+    const user = await $('//*[@id="user-label"]').getText();
+    if (user !== 'John Walker') throw new Error('invalid user');
+  });
+
+  it('should check menu item', async function () {
+    const menuItems = await $$(
+      '//*[@id="first-nav-block"]//*[contains(@class, "nav-item")]'
+    );
+    for (item of menuItems) {
+      await item.moveTo();
+      bgc = await item.getCSSProperty('background-color');
+      if ((bgc = 'red')) browser.pause();
+      throw new Error(`Background color on hover red`);
+    }
+  });
 });
-
-
