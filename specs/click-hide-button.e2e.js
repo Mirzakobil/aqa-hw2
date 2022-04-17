b3db289ed381a617e2ce82d3bf34fa0dcf0e668b
Mirzakobil <mirzakobilanvarov@gmail.com>
2022-04-17 12:16:22 +0500

    part 2

--- specs/click-hide-button.e2e.js
+++ specs/click-hide-button.e2e.js
@@ -1,14 +1,25 @@
 // REMOVE THE BELOW CODE BEFORE START THE EXERCISE
 describe('Check app', function () {
-    it('click on hide button', async function () {
-        await browser.url('https://viktor-silakov.github.io/course-sut');
-        await $('#login').setValue('walker@jw.com');
-        await $('#password').setValue('password');
-        await $('button').click();
-        await browser.pause(3000);
-        await browser.execute('alert(arguments[0] + arguments[1])', 'Hello', ' World!');
-        await browser.pause(3000);
-    });
+  it('click on hide button', async function () {
+    await browser.url('https://viktor-silakov.github.io/course-sut');
+    await $('#login').setValue('walker@jw.com');
+    await $('#password').setValue('password');
+    await $('button').click();
+    // method 1
+    await browser.execute(
+      'document.getElementsByClassName(arguments[0])[0].remove()',
+      'sticky-top'
+    );
+    await browser.execute(
+      'document.getElementsByClassName(arguments[0])[0].click()',
+      'btn-danger'
+    );
+    // method 2
+    // await browser.execute(
+    //   'document.getElementsByClassName("sticky-top")[0].remove()'
+    // );
+    // await browser.execute(
+    //   'document.getElementsByClassName("btn-danger")[0].click()'
+    // );
+  });
 });
-
-
