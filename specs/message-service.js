f8d43ff69c7842501841d2fb5d11d96f084d9331
Mirzakobil <mirzakobilanvarov@gmail.com>
2022-04-17 17:06:05 +0500

    part 1

--- specs/message-service.js
+++ specs/message-service.js
@@ -31,48 +31,49 @@ function stopAllNodes() {
   stopClientPC();
 }
 
-describe('Message Sending', function () {
-  it('should send message to Mars without error', function () {
-    let tokens = startAllNodes();
-    const response = sendMessage('Hello', 'Mars', tokens.mars);
-    assertResponse(response, 'Success');
-    stopAllNodes();
+describe('Message sending', function () {
+  context('Message to Earth', function () {
+    it('should send message to Earth without error', function () {
+      let tokens = startAllNodes();
+      const response = sendMessage('Hello', 'Earth', tokens.earth);
+      assertResponse(response, 'Success');
+      stopAllNodes();
+    });
+    it('should send message to Earth with "Security Error"', function () {
+      startAllNodes();
+      const response = sendMessage('Hello', 'Earth', 'X0000');
+      assertResponse(response, 'Security Error');
+      stopAllNodes();
+    });
   });
+  context('Message to Mars', function () {
+    it('should send message to Mars without error', function () {
+      let tokens = startAllNodes();
+      const response = sendMessage('Hello', 'Mars', tokens.mars);
+      assertResponse(response, 'Success');
+      stopAllNodes();
+    });
+    it('should send message to Mars with "Security Error"', function () {
+      startAllNodes();
+      const response = sendMessage('Hello', 'Mars', 'z0101');
+      assertResponse(response, 'Security Error');
+      stopAllNodes();
+    });
 
-  it('should send message to Earth without error', function () {
-    let tokens = startAllNodes();
-    const response = sendMessage('Hello', 'Earth', tokens.earth);
-    assertResponse(response, 'Success');
-    stopAllNodes();
-  });
+    it('should send message to Mars with valid token and "Service is unavailable" error', function () {
+      startAllNodes();
+      stopSatelite();
+      const response = sendMessage('Hello', 'Mars', startMarsServer());
+      assertResponse(response, 'Service is unavailable');
+      stopAllNodes();
+    });
 
-  it('should send message to Earth with "Security Error"', function () {
-    startAllNodes();
-    const response = sendMessage('Hello', 'Earth', 'X0000');
-    assertResponse(response, 'Security Error');
-    stopAllNodes();
-  });
-
-  it('should send message to Mars with "Security Error"', function () {
-    startAllNodes();
-    const response = sendMessage('Hello', 'Mars', 'z0101');
-    assertResponse(response, 'Security Error');
-    stopAllNodes();
-  });
-
-  it('should send message to Mars with valid token and "Service is unavailable" error', function () {
-    startAllNodes();
-    stopSatelite();
-    const response = sendMessage('Hello', 'Mars', startMarsServer());
-    assertResponse(response, 'Service is unavailable');
-    stopAllNodes();
-  });
-
-  it('should send message to Mars with "Security Error" and "Service is unavailable" error', function () {
-    startAllNodes();
-    stopSatelite();
-    const response = sendMessage('Hello', 'Mars', 'M1021');
-    assertResponse(response, 'Service is unavailable' || 'Security Error');
-    stopAllNodes();
+    it('should send message to Mars with "Security Error" and "Service is unavailable" error', function () {
+      startAllNodes();
+      stopSatelite();
+      const response = sendMessage('Hello', 'Mars', 'M1021');
+      assertResponse(response, 'Service is unavailable' || 'Security Error');
+      stopAllNodes();
+    });
   });
 });
